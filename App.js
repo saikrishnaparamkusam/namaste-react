0c3c783bed9037c87c8af088784a35722ca81b21
saikrishnaparamkusam <mailtosaikrishnaa@gmail.com>
2023-09-28 11:35:29 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement("div", {id:"parent"}, 
+    React.createElement("div", {id:"child"}, 
+    [
+        React.createElement("h1", {id:"heading"}, "Hello world from nested HTML" ),
+        React.createElement("h1", {id:"heading"}, "Another H1 tag")
+    ]
+    )
+);
+
+
+
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(parent);
